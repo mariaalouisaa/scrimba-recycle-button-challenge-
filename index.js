425fdbc4edf16c7a607a858bb0800c7d2bc80a03
= <maria.louisa.castro@gmail.com>
2022-04-21 20:41:26 +0100

    Add clear button functionality

--- index.js
+++ index.js
@@ -10,15 +10,21 @@ function recycle() {
   if (recycleBin.innerText.length < 198) {
     recycleBin.innerText = recycleBin.innerText + recycleSelect.value;
   } else {
-    recycleBtn.innerHTML = "BIN FULL";
+    recycleBtn.innerHTML = "bin full";
     recycleBtn.style.background = "red";
   }
 }
 
-// Task: Wire up the <select> tag and recycle button so that an emoji of the selected item appears in the DOM each time the recycle button is pressed.
+function clear() {
+  recycleBin.innerHTML = "";
+  recycleBtn.innerHTML = "recycle";
+  recycleBtn.style.background = "var(--raisin-black)";
+}
 
-// stretch goal 1️⃣ Add an item limit to indicate when the recycle bin is full
+// ✔️ Task: Wire up the <select> tag and recycle button so that an emoji of the selected item appears in the DOM each time the recycle button is pressed.
 
-// stretch goal 2️⃣: Add the option to empty the bin.
+// ✔️ stretch goal 1️⃣ Add an item limit to indicate when the recycle bin is full
+
+// ✔️ stretch goal 2️⃣: Add the option to empty the bin.
 
 // stretch goal 3️⃣: Animate adding the items to the bin.
